edcd01569c10f9576fcc107de097e16a4141ca39
aikoextended <putrimeyliyaalfath@gmail.com>
2024-12-12 01:37:41 +0700

    update

--- database/migrations/000002_create_genres_table.up.sql
+++ database/migrations/000002_create_genres_table.up.sql
@@ -2,3 +2,20 @@ CREATE TABLE genres (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255)
 );
+
+INSERT INTO genres (name) 
+VALUES
+    ('Action'),
+    ('Adventure'),
+    ('Comedy'),
+    ('Drama'),
+    ('Fantasy'),
+    ('Magic'),
+    ('Mecha'),
+    ('Music'),
+    ('Romance'),
+    ('Sci-Fi'),
+    ('Shounen'),
+    ('Slice of Life'),
+    ('Sports'),
+    ('Supernatural');
--- database/migrations/000004_create_animes_table.up.sql
+++ database/migrations/000004_create_animes_table.up.sql
@@ -8,6 +8,16 @@ CREATE TABLE animes (
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
+INSERT INTO animes (title, genre_id, sinopsis, episodes)
+VALUES
+    ('Attack on Titan', 1, 'Humanity fights for survival against man-eating giants.', 87),
+    ('One Piece', 2, 'Follow the adventures of Luffy and his pirate crew in search of the ultimate treasure.', 1050),
+    ('Naruto', 3, 'A young ninja strives to become the Hokage while protecting his village.', 720),
+    ('Demon Slayer', 4, 'A young boy becomes a demon slayer to avenge his family and save his sister.', 26),
+    ('My Hero Academia', 1, 'In a world where superpowers are the norm, a boy without powers aspires to be a hero.', 138),
+    ('Your Lie in April', 12, 'A young pianist rediscovers his love for music through a spirited violinist.', 22);
+
+
 -- CREATE TABLE animes (
 --     id AUTO_INCREMENT PRIMARY KEY,
 --     title VARCHAR(255) NOT NULL,
